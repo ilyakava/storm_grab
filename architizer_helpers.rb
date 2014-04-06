76afb7ad58dcf320a1d5a39ffe7a1b60c9231d7d
Catherine Finsness <cfinsness@gmail.com>
2014-04-06 17:40:27 -0400

    Added key words to exclude from Admin search

--- architizer_helpers.rb
+++ architizer_helpers.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 module ArchitizerHelpers
 
-  STOP_WORDS = ["Architecture" "Architects" "Architekten" "Architectes" "architect" "LLP" "Associates" "Arhitekti" "arquitectura" "Arkitekter" "Arquitectos" "Arquitecto" "Architekti" "&" "and" ","]
+  STOP_WORDS = %w{Architecture Architects Architekten Architectes architect LLP Associates Arhitekti arquitectura Arkitekter Arquitectos Arquitecto Architekti & and , + Associates} 
 
   def is_stop_word?(word)
     STOP_WORDS.any? { |w| word.match %r{#{w}}i }
