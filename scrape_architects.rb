e11127eaf8bbcc753d2da0ddb6465777aa121def
Ilya Kavalerov <ilya@artsymail.com>
2014-05-29 15:44:10 -0400

    Adds but doesn't integrate a scraper method for another architecture site

--- scrape_architects.rb
+++ scrape_architects.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'erb'
 require_relative './architizer_helpers'
+require 'csv'
 
 class ScrapeArchitects
 
@@ -50,6 +51,41 @@ class ScrapeArchitects
     end
   end
 
+  def scrape_projektydomov(last_page = 50)
+    @website_data["projektydomov"] = {}
+
+    (1..last_page).each do |page_num|
+      doc = Nokogiri::HTML(open("http://www.projektydomov.eu/en/list-of-companies/architects/#{page_num}/"))
+      # there are many identical nodes for each title, the first of a series of nodes
+      # contains architect information, the next identical are discarded, hence the hash
+      nodes = doc.css('div[itemtype="http://schema.org/Organization"]').reverse
+      names = nodes.map do |node|
+        node.css('div.txt > h2 > a > span').first.text
+      end
+      locations = nodes.map do |node|
+        node.css('div.txt > p.loc').children.map(&:text).join
+      end
+      websites = nodes.map do |node|
+        node.css('div.txt > ul.list_3 > li > a').text
+      end
+      emails = nodes.map do |node|
+        node.css('div.txt > ul.list_3 > li.l > a').first.attributes["href"].value.sub('mailto:', '')
+      end
+      nodes.length.times do |i|
+        @website_data["projektydomov"][names[i]] = {
+          "name" => names[i],
+          "google_search" => "https://www.google.com/#q=#{google_queryify(names[i])}",
+          "architizer_search" => "http://architizer.com/search/q/q:#{architizer_queryify(names[i])}/",
+          "website" => websites[i],
+          "location" => locations[i],
+          "email" => emails[i],
+          "source" => "projektydomov #{page_num}"
+        }
+      end
+      puts "#{(100 * page_num / last_page.to_f).floor} % of projektydomov done"
+    end
+  end
+
   def build_html
     ERB.new(File.read("blog_condensation.rhtml"), 0, "", "@html").result binding
   end
@@ -57,6 +93,18 @@ class ScrapeArchitects
   def export_html(outfile_name = "table.html")
     File.open(outfile_name, "w") do |io|
       io << build_html
+    end
+  end
+
+  def export_csv
+    @website_data.each_pair do |website, all_data|
+      CSV.open("#{website}.csv", 'w:UTF-8') do |f|
+        name, data = all_data.first
+        f << data.keys
+        all_data.each_pair do |name, data|
+          f<< data.values.map { |e| e.force_encoding(Encoding.find("UTF-8")) }
+        end
+      end
     end
   end
 end
